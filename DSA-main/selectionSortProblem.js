918e583c6738b5265ed4d910001267b0da459e14
VIKASH LAL <105917542+Therobo77@users.noreply.github.com>
2024-02-12 14:32:11 +0530

    Update selectionSortProblem.js

--- DSA-main/selectionSortProblem.js
+++ DSA-main/selectionSortProblem.js
@@ -2,7 +2,9 @@ function solve(arr) {
     for (let i = 0; i < arr.length; i++) {
         let min = i;
         for (let j = i + 1; j < arr.length; j++) {
-            if (arr[j] < arr[min]) min = j;
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
         }
         [arr[i], arr[min]] = [arr[min], arr[i]];
     }
